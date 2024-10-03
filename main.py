493452a9612c17dc59500c4395bb9215ce0fd997
Bellika <markusbellika@gmail.com>
2024-10-03 19:01:57 +0200

    computer option done

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 class Player: 
   def __init__(self):
     self.valid_options = ["sten", "sax", "påse"]
@@ -10,5 +11,13 @@ class Player:
 
     print(f"Du valde: {self.option}")
 
+class Computer:
+  def __init__(self):
+    self.valid_options = ["sten", "sax", "påse"]
+    self.option = random.choice(self.valid_options)
+
+    print(f"{self.option}")
+
 if __name__ == "__main__":
   player = Player()
+  computer = Computer()
